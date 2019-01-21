518443ece8321f0611c9a9346d816ccbe7e16a41
ccdle12 <chris.coverdale24@gmail.com>
2019-01-21 11:05:29 -0500

    API_strings: adding scripts to dump API strings
    This PR adds a script that will dump a hex of the admin.macaroon and a
    cert thumb print of the tls.cert. Both these strings are needed for
    connection to the LND Node via REST. This was made specifically in mind
    for connecting a external LND Node to BTCPayServer but can also be used
    for connecting to Lighting Apps.

---
+++ API_strings.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Get the lightning chain environment.
+testnet=""
+testnet=$(. lncli.sh getinfo | grep true)
+
+# Macaroon hex dump.
+macaroon=""
+
+# If testnet variable is empty, meaning it is mainnet.
+if [ -z "$testnet" ]
+then
+    macaroon=$(docker exec -ti bitcoin_node /bin/bash -c "
+        chain=$(xxd -ps -u -c 1000  /root/.lnd/data/chain/bitcoin/mainnet/admin.macaroon)
+    ")
+else
+    macaroon=$(docker exec -ti bitcoin_node /bin/bash -c "
+        xxd -ps -u -c 1000  /root/.lnd/data/chain/bitcoin/testnet/admin.macaroon
+    ")
+fi
+
+# certthumpprint of the tls.cert for communication.
+cert_thumb_print=""
+
+cert_thumb_print=$(docker exec -ti bitcoin_node /bin/bash -c "
+        openssl x509 -noout -fingerprint -sha256 -inform pem -in /root/.lnd/tls.cert
+    ")
+
+echo ""
+echo "**** MACAROON AND TLS CERT THUMB PRINT ****"
+echo ""
+echo "MACAROON: "$macaroon
+echo ""
+echo $cert_thumb_print
+echo ""
